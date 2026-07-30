6e906275c1c4dc896faed334047786e0a1c60452
Cardani Dev <cardani.dev@gmail.com>
2026-07-30 05:26:10 -0500

    Módulo de vehículos v1 terminado

--- js/vehiculos.js
+++ js/vehiculos.js
@@ -5,25 +5,29 @@ const listaVehiculos = document.getElementById("listaVehiculos");
 
 let vehiculoEnEdicion = null;
 
-//submit 
+//Capturar el envío del formulario 
 formularioVehiculo.addEventListener("submit", async function(evento){
-//preventDefault
+//Evitar recarga de la página
     evento.preventDefault();
-//obtener datos
+//Obtener datos del formulario
     const placa = document.getElementById("placa").value;
     const marca = document.getElementById("marca").value;
     const modelo = document.getElementById("modelo").value;
     const color = document.getElementById("color").value;
-    const anioFabricacion = document.getElementById("anioFabricacion").value;
+    let anioFabricacion = document.getElementById("anioFabricacion").value;
+
+        if (anioFabricacion === "") {
+        anioFabricacion = null;
+        }
     const cliente = document.getElementById("cliente").value;
-//validar
+//Validar datos
     if(placa ==="" || marca ==="" || modelo ==="" || cliente ==="") {
         alert("Debe seleccionar un cliente y completar placa, marca, y modelo.");
         return;
     }
 
     let error;
-//insert
+//Registrar o actualizar vehículo
     if(vehiculoEnEdicion === null) {
         const resultado = await supabaseClient
             .from("vehiculos")
@@ -40,7 +44,7 @@ formularioVehiculo.addEventListener("submit", async function(evento){
             ]);
 
         error = resultado.error;
-//update
+
     } else {
         const resultado = await supabaseClient
             .from("vehiculos")
@@ -59,7 +63,7 @@ formularioVehiculo.addEventListener("submit", async function(evento){
             error = resultado.error;
 
     }
-//verificar error
+//Verificar si ocurrió un error 
 
     if(error){
 
@@ -69,20 +73,143 @@ formularioVehiculo.addEventListener("submit", async function(evento){
 
     }
 
-//actualizar lista
+//actualizar la interfaz
 
     obtenerVehiculos();
 
-//alerta
 
     alert("Vehículo guardado correctamente");
 
-//reset
+//limpiar formulario 
 
     formularioVehiculo.reset();
 
-//volver al modo registrar
+//Restablecer estado del formulario
 
     vehiculoEnEdicion = null;
     btnVehiculo.textContent = "Agregar Vehículo"
 });
+
+async function obtenerVehiculos() {
+    const { data, error } = await supabaseClient
+        .from("vehiculos")
+        .select("*");
+
+    if (error) {
+        console.log(error);
+        alert("Error al obtener los vehiculos registrados.")
+        return;
+    }
+
+    mostrarVehiculos(data);
+
+}
+
+function mostrarVehiculos(vehiculos) {
+
+    listaVehiculos.innerHTML = "";
+
+    vehiculos.forEach(function(vehiculo){
+
+        const tarjeta = document.createElement("div");
+
+        tarjeta.innerHTML = `
+        <p><strong>Placa:</strong>${vehiculo.placa}</p>
+        <p><strong>Marca:</strong>${vehiculo.marca}</p>
+        <p><strong>Modelo:</strong>${vehiculo.modelo}</p>
+        <p><strong>Color:</strong>${vehiculo.color || "No registrado"}</p>
+        <p><strong>Año:</strong>${vehiculo.anio_fabricacion || "No registrado"}</p>
+        
+            <button onclick="editarVehiculo(${vehiculo.id})">Editar</button>
+        
+        
+            <button onclick="eliminarVehiculo(${vehiculo.id})">Eliminar</button>
+        
+        `;
+
+        listaVehiculos.appendChild(tarjeta);
+
+    });
+
+}
+
+async function editarVehiculo(id) {
+
+    const { data, error } = await supabaseClient
+    .from("vehiculos")
+    .select("*")
+    .eq("id", id)
+    .single();
+    
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    document.getElementById("placa").value = data.placa
+    document.getElementById("marca").value = data.marca;
+    document.getElementById("modelo").value = data.modelo;
+    document.getElementById("color").value = data.color;
+    document.getElementById("anioFabricacion").value = data.anio_fabricacion;
+    selectCliente.value = data.cliente_id;
+
+    vehiculoEnEdicion = id;
+    btnVehiculo.textContent = "Actualizar vehículo";
+    
+
+}
+
+async function eliminarVehiculo(id) {
+
+    const confirmar = confirm("¿Está seguro de que desea eliminar este vehículo?")
+
+    if (!confirmar) {
+        return;
+    }
+    
+    const{ error } = await supabaseClient
+        .from("vehiculos")
+        .delete()
+        .eq("id", id);
+
+        if(error) {
+            console.log(error);
+            alert("Error al eliminar vehículo.")
+            return;
+        } 
+
+        alert("Vehículo eliminado correctamente.");
+
+        obtenerVehiculos();
+}
+
+async function cargarClientes() {
+    
+    const { data, error } = await supabaseClient
+        .from("clientes")
+        .select("*");
+
+        if(error) {
+            console.log(error);
+            return;
+        }
+        selectCliente.innerHTML = `
+            <option value="">Seleccione un cliente</option>
+        `;
+
+       
+
+        data.forEach(function(cliente){
+
+             const opcion = document.createElement("option");
+
+            opcion.value = cliente.id;
+            opcion.textContent = cliente.nombre;
+
+            selectCliente.appendChild(opcion);
+
+        });
+}
+
+cargarClientes();
+obtenerVehiculos();
