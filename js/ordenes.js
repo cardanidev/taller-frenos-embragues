deb72cd6a3f134fa341587d1e9c3a8e91e0966c5
Cardani Dev <cardani.dev@gmail.com>
2026-09-02 06:20:50 -0500

    Completa flujo de estados de ordenes de trabajo

--- js/ordenes.js
+++ js/ordenes.js
@@ -10,23 +10,20 @@ let ordenEnEdicion = null;
 
 async function cargarClientes() {
 
-    console.log("cargarClientes ejecutándose")
-
     const { data, error } = await supabaseClient
         .from("clientes")
         .select("*");
 
-        if(error){
+        if (error) {
             console.log(error);
             return;
         }
-        console.log(data);
         selectCliente.innerHTML = `
             <option value = "">Seleccione Un Cliente</option> 
         `;
 
 
-        data.forEach(function(cliente){
+        data.forEach(function(cliente) {
 
             const opcion = document.createElement("option");
 
@@ -40,7 +37,7 @@ async function cargarClientes() {
         
 }
 
-selectCliente.addEventListener("change", async function (evento){
+selectCliente.addEventListener("change", async function (evento) {
 
     const clienteId = selectCliente.value;
 
@@ -49,16 +46,17 @@ selectCliente.addEventListener("change", async function (evento){
         .select("*")
         .eq("cliente_id", clienteId);
 
-        console.log("vehiculos encontrados", data)
-
-        console.log(data);
+        if (error) {
+            console.log(error);
+            return;
+        }
 
         selectVehiculo.innerHTML = `
             <option value = "">Seleccione Un Vehículo</option>
         `;
 
     
-    data.forEach(function(vehiculo){
+    data.forEach(function(vehiculo) {
 
         const option = document.createElement("option");
 
@@ -73,14 +71,14 @@ selectCliente.addEventListener("change", async function (evento){
 
 });
 
-formularioOrdenes.addEventListener("submit", async function(evento){
+formularioOrdenes.addEventListener("submit", async function(evento) {
     
     evento.preventDefault();
 
     const vehiculoId = selectVehiculo.value;
     const diagnostico = diagnosticoCliente.value;
     
-    if (ordenEnEdicion === null){
+    if (ordenEnEdicion === null) {
 
          const nuevaOrden = {
 
@@ -94,21 +92,17 @@ formularioOrdenes.addEventListener("submit", async function(evento){
         .from("ordenes_trabajo")
         .insert(nuevaOrden);
 
-    if(error){
+    if (error) {
         console.log(error);
         alert("Error Al Crear La Orden")
         return;
 
     } else {
-
-            console.log(data);
             alert("Orden Creada");
 
             formularioOrdenes.reset();
 
             obtenerOrdenes();
-
-            
     }
 
     } else {
@@ -151,11 +145,9 @@ async function obtenerOrdenes() {
         return;
     }
 
-    console.log(data);
-
     listaOrdenes.innerHTML = "";
 
-    data.forEach(function(orden){
+    data.forEach(function(orden) {
         
 
         const fecha = new Date(orden.fecha_ingreso);
@@ -174,6 +166,37 @@ async function obtenerOrdenes() {
 
         }
 
+        let botonEstado = "";
+
+        if (orden.estado === "pendiente") {
+            botonEstado = `
+                <button type="button" class="btn-estado" data-id="${orden.id}"> Iniciar Proceso</button>
+            `;
+        }
+
+        if (orden.estado === "en proceso") {
+
+            botonEstado = `
+                <button type="button" class="btn-estado" data-id="${orden.id}">Terminar Orden</button>
+
+                <button type="button" class="btn-abortar" data-id="${orden.id}">Abortar Orden</button>
+            `;
+
+        }
+
+        if (orden.estado === "terminada") {
+            botonEstado = `
+            <button type="button" class="btn-entregar" data-id="${orden.id}">Entregar Orden</button>
+            `
+        }
+
+        let botonEditar = "";
+        if (orden.estado !== "terminada" && orden.estado !== "entregada" && orden.estado !== "abortada") {
+            botonEditar = `
+            <button type= "button" class= "btn-editar" data-id="${orden.id}">Editar</button>
+            `;
+        }
+
         divOrden.innerHTML = `
         
             <h3>Orden #${orden.id}</h3>
@@ -182,22 +205,21 @@ async function obtenerOrdenes() {
             <p>Diagnóstico: ${orden.diagnostico_cliente}</p>
             <p>Estado: ${orden.estado}</p>
             <p>Fecha: ${fechaFormateada}</p>
-            <button type= "button" class="btn-editar" data-id="${orden.id}">Editar</button>
+            ${botonEditar}
             ${botonEliminar}
+            ${botonEstado} 
          
         `;
 
         const btnEliminar = divOrden.querySelector (".btn-eliminar");
-            console.log(btnEliminar);
+            
         if (btnEliminar) {
 
-            btnEliminar.addEventListener("click", async function(){
-
-                console.log("Eliminar orden", this.dataset.id);
+            btnEliminar.addEventListener("click", async function() {
 
                 const ordenId = this.dataset.id;
                 
-                if(!confirm("Estás seguro de eliminar esta orden?")){
+                if(!confirm("Estás seguro de eliminar esta orden?")) {
                     return;
                 }
 
@@ -207,7 +229,7 @@ async function obtenerOrdenes() {
                     .delete()
                     .eq("id", ordenId);
 
-                    if(error) {
+                    if (error) {
                         console.log(error);
                         alert("Error Al Eliminar La Orden")
                         return;
@@ -222,12 +244,102 @@ async function obtenerOrdenes() {
 
         const btnEditar = divOrden.querySelector(".btn-editar");
 
+        if (btnEditar) {
         btnEditar.addEventListener("click", function() {
                 ordenEnEdicion= this.dataset.id;
-                console.log(ordenEnEdicion);
+
                 diagnosticoCliente.value = orden.diagnostico_cliente;
                 btnOrden.textContent = "Actualizar Orden";
         });
+    }
+
+        const btnEstado = divOrden.querySelector(".btn-estado");
+
+        if (btnEstado) {
+            btnEstado.addEventListener("click", async function () {
+                const ordenId = this.dataset.id;
+
+                let nuevoEstado; 
+
+                if (orden.estado === "pendiente") {
+                    nuevoEstado = "en proceso";
+                }
+
+                if (orden.estado === "en proceso") {
+                    nuevoEstado = "terminada"
+                }
+               
+                const { data, error } = await supabaseClient
+
+                .from("ordenes_trabajo")
+                .update({
+                    estado: nuevoEstado
+                })
+                .eq("id", ordenId);
+
+                if (error) {
+                    console.log(error);
+                    alert("Error Al Cambiar El Estado");
+                    return;
+                }
+
+                alert("Estado actualizado")
+                obtenerOrdenes();
+            });
+        }
+
+        const btnAbortar = divOrden.querySelector(".btn-abortar");
+
+        if (btnAbortar) {
+            btnAbortar.addEventListener("click", async function () {
+
+                const ordenId = this.dataset.id;
+
+                const { data, error } = await supabaseClient
+                .from("ordenes_trabajo")
+                .update({
+                    estado: "abortada"
+                })
+                .eq("id", ordenId);
+
+                if (error) {
+                    console.log(error);
+                    alert("Error Al Abortar La Orden");
+                    return;
+                }
+                alert("Orden Abortada");
+                obtenerOrdenes();
+
+            });
+
+        }
+
+        const btnEntregar = divOrden.querySelector(".btn-entregar");
+
+        if (btnEntregar) {
+
+            btnEntregar.addEventListener("click", async function() {
+                const ordenId = this.dataset.id;
+
+                const { data, error } = await supabaseClient
+
+                .from("ordenes_trabajo")
+                .update({
+                    estado: "entregada"
+                })
+                .eq("id", ordenId);
+
+                if (error) {
+                    console.log(error);
+                    alert("Error Al Entregar La Orden");
+                    return;
+                }
+                alert("Orden Entregada");
+                obtenerOrdenes();
+
+            });
+
+        }
 
         listaOrdenes.appendChild(divOrden);
 
